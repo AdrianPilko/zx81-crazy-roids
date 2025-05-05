673f58061286c9476a4dc33a019dbf62e8d282e5
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2025-05-05 14:55:36 +0100

    Update drawAsteroids.asm

--- drawAsteroids.asm
+++ drawAsteroids.asm
@@ -2,46 +2,54 @@
 
 drawAsteroid
 
-    ;check if asteroid is valid
-    ld a, $fe
-    ld b, a
-    ld a, (asteroidValidBitMap)
-    cp b
-    jp z, skipDrawAsteroid
-
-
-    ;ld de, (asteroidTopLeftPositions)  ;; should be doing this in the drawAsteroid subroutine
-    ld hl, (asteroidTopLeftPositions)  ;; should be doing this in the drawAsteroid subroutine
-    ld de, -33
-    add hl, de
-    push hl
-    pop de 
-    ld hl, blankSprite
-    ld c, 4
+assumeAllAsteroidsValid
     ld b, 1
-    call drawSprite
 
-    ld de, (asteroidTopLeftPositions)
-    ld hl, (asteroidSpritePointer)
-    ld b, 4
-    ld c, 4 
-    call drawSprite
+drawAsteroidLoop
+    push bc
+        ;check if asteroid is valid
+        ld a, $fe
+        ld b, a
+        ld a, (asteroidValidBitMap)
+        cp b
+        jp z, skipDrawAsteroid
 
-    ld a, (asteroidSpriteCycleCount)
-    inc a
-    cp 2
-    jr z, resetAsteroidSprite
-    ld (asteroidSpriteCycleCount), a
-    ld hl, (asteroidSpritePointer)
-    ld de, 32
-    add hl, de
-    ld (asteroidSpritePointer), hl
-    ret
+        ;ld de, (asteroidTopLeftPositions)  ;; should be doing this in the drawAsteroid subroutine
+        ld hl, (asteroidTopLeftPositions)  ;; should be doing this in the drawAsteroid subroutine
+        ld de, -33
+        add hl, de
+        push hl
+        pop de 
+        ld hl, blankSprite
+        ld c, 4
+        ld b, 1
+        call drawSprite
+
+        ld de, (asteroidTopLeftPositions)
+        ld hl, (asteroidSpritePointer)
+        ld b, 4
+        ld c, 4 
+        call drawSprite
+
+        ld a, (asteroidSpriteCycleCount)
+        inc a
+        cp 2
+        jr z, resetAsteroidSprite
+        ld (asteroidSpriteCycleCount), a
+        ld hl, (asteroidSpritePointer)
+        ld de, 32
+        add hl, de
+        ld (asteroidSpritePointer), hl
+        jr skipDrawAsteroid
+
 resetAsteroidSprite
-    xor a
-    ld (asteroidSpriteCycleCount), a
-    ld hl, asteroidSpriteData4x4
-    ld (asteroidSpritePointer), hl    
-skipDrawAsteroid
+        xor a
+        ld (asteroidSpriteCycleCount), a
+        ld hl, asteroidSpriteData4x4
+        ld (asteroidSpritePointer), hl    
+    skipDrawAsteroid
+    pop bc 
+    djnz drawAsteroidLoop
+
     ret   
 
